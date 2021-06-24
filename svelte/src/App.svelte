ffa3e5d483b2431b0d99db46f5edd02581e0eb33
Tatsushi Toji <28854043+tatsushitoji@users.noreply.github.com>
2021-06-25 00:24:07 +0900

    feat: implement simple Counter

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	export let name: string;
+	import Counter from './/Counter.svelte';
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
+<Counter />
 
 <style>
 	main {
---
+++ svelte/src/Counter.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let count = 0;
+
+	const onDecreaseClick = () => (count -= 1)
+  const onIncreaseClick = () => (count += 1)
+</script>
+
+<div class="counter">
+	<div class="counter-viewport">
+		{count}
+	</div>
+
+	<button on:click={onDecreaseClick} aria-label="Decrease the counter by one">
+		-1
+	</button>
+
+	<button on:click={onIncreaseClick} aria-label="Increase the counter by one">
+		+1
+	</button>
+</div>
